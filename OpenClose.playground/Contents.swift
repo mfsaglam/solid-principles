a2059a745f9833547c0a48ba4ed92d5d6058335d
mfsaglam <mfsaglam1@gmail.com>
2023-04-26 17:56:44 +0300

    add filterByColor method

--- OpenClose.playground/Contents.swift
+++ OpenClose.playground/Contents.swift
@@ -23,3 +23,35 @@ class Product {
         self.size = size
     }
 }
+
+class ProductFilter {
+    func filterByColor(products: [Product], color: Color) -> [Product] {
+        var result = [Product]()
+        
+        for p in products {
+            if p.color == color {
+                result.append(p)
+            }
+        }
+        
+        return result
+    }
+}
+
+func main() {
+    let apple = Product("apple", .green, .small)
+    let tree = Product("tree", .green, .large)
+    let house = Product("house", .blue, .large)
+    
+    let products = [apple, tree, house]
+    
+    let pf = ProductFilter()
+    
+    print("Green products(old):")
+    
+    for p in pf.filterByColor(products: products, color: .green) {
+        print("- \(p.name) is green.")
+    }
+}
+
+main()
