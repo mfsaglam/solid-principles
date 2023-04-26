7de206735b9be61280207c768cfe7204509d69fd
mfsaglam <mfsaglam1@gmail.com>
2023-04-26 19:37:40 +0300

    add BetterFilter and implement it

--- OpenClose.playground/Contents.swift
+++ OpenClose.playground/Contents.swift
@@ -59,8 +59,34 @@ protocol Specification {
 
 protocol Filter {
     associatedtype T
-    func filter<Spec: Specification>(_ items: [T], spec: Spec) -> [T]
-        where Spec.T == T;
+    func filter<Spec: Specification>(_ items: [T], spec: Spec) -> [T] where Spec.T == T
+}
+
+class ColorSpecification: Specification {
+    typealias T = Product
+    let color: Color
+    
+    init(color: Color) {
+        self.color = color
+    }
+    func isSatisfied(_ item: Product) -> Bool {
+        item.color == color
+    }
+}
+
+class BetterFilter: Filter {
+    typealias T = Product
+
+    func filter<Spec: Specification>(_ items: [Product], spec: Spec) -> [Product] where Spec.T == T {
+        var result = [Product]()
+        
+        for i in items {
+            if spec.isSatisfied(i) {
+                result.append(i)
+            }
+        }
+        return result
+    }
 }
 
 func main() {
@@ -71,10 +97,17 @@ func main() {
     let products = [apple, tree, house]
     
     let pf = ProductFilter()
+    let bf = BetterFilter()
     
     print("Green products(old):")
     
     for p in pf.filterByColor(products: products, color: .green) {
+        print("- \(p.name) is green.")
+    }
+    
+    print("Green products(new):")
+    
+    for p in bf.filter(products, spec: ColorSpecification(color: .green)) {
         print("- \(p.name) is green.")
     }
 }
