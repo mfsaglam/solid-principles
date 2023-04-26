473e8d15a24c5e0a564233a27cf67d27b882d79a
mfsaglam <mfsaglam1@gmail.com>
2023-04-26 16:22:59 +0300

    Initial Commit

---
+++ SOLID/SOLID.h
@@ -0,0 +1,18 @@
+//
+//  SOLID.h
+//  SOLID
+//
+//  Created by Fatih Sağlam on 26.04.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SOLID.
+FOUNDATION_EXPORT double SOLIDVersionNumber;
+
+//! Project version string for SOLID.
+FOUNDATION_EXPORT const unsigned char SOLIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOLID/PublicHeader.h>
+
+
