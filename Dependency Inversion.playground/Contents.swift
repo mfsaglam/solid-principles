0c23dffd41b7a49599ef11e98d0e0dbe70b639e0
mfsaglam <mfsaglam1@gmail.com>
2023-04-27 18:26:28 +0300

    define an interface `RelationshipBrowser` that only exposes the methods required by the `Research` class to access the `Relationships` data, and have the `Relationships` class implement that interface. The `Research` class can then depend on the `RelationshipBrowser` interface instead of the `Relationships` class directly.

--- Dependency Inversion.playground/Contents.swift
+++ Dependency Inversion.playground/Contents.swift
@@ -14,20 +14,37 @@ class Person {
     }
 }
 
-class Relationships { // low-level
+protocol RelationshipBrowser {
+    func findAllChildrenOf(_ name: String) -> [Person]
+}
+
+class Relationships: RelationshipBrowser {
+    // low-level
     var relations = [(Person, Relationship, Person)]()
     
     func addParentAndChild(_ p: Person, _ c: Person) {
         relations.append((p, .parent, c))
         relations.append((c, .child, p))
     }
+    
+    func findAllChildrenOf(_ name: String) -> [Person] {
+        relations
+            .filter { $0.name == name && $1 == .parent && $2 === $2}
+            .map { $2 }
+    }
 }
 
 class Research { // high-level
-    init(_ relationships: Relationships) {
-        let relations = relationships.relations
-        for r in relations where r.0.name == "John" && r.1 == .parent {
-            print("John has a child called \(r.2.name)")
+//    init(_ relationships: Relationships) {
+//        let relations = relationships.relations
+//        for r in relations where r.0.name == "John" && r.1 == .parent {
+//            print("John has a child called \(r.2.name)")
+//        }
+//    }
+    
+    init(_ browser: RelationshipBrowser) {
+        for p in browser.findAllChildrenOf("John") {
+            print("John has a child called \(p.name)")
         }
     }
 }
@@ -40,6 +57,8 @@ func main() {
     let relationships = Relationships()
     relationships.addParentAndChild(parent, child1)
     relationships.addParentAndChild(parent, child2)
+    
+    let _ = Research(relationships)
 }
 
 main()
