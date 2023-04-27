aedaaa970c2aa9d55f8bbcd1465084efdda6d416
mfsaglam <mfsaglam1@gmail.com>
2023-04-27 18:17:40 +0300

    The code violates the Dependency Inversion Principle since the high-level Research class directly depends on the low-level Relationships class, making it difficult to modify or extend the code in the future.

---
+++ Dependency Inversion.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+enum Relationship {
+    case parent
+    case child
+    case sibling
+}
+
+class Person {
+    var name = ""
+    
+    init(_ name: String) {
+        self.name = name
+    }
+}
+
+class Relationships { // low-level
+    var relations = [(Person, Relationship, Person)]()
+    
+    func addParentAndChild(_ p: Person, _ c: Person) {
+        relations.append((p, .parent, c))
+        relations.append((c, .child, p))
+    }
+}
+
+class Research { // high-level
+    init(_ relationships: Relationships) {
+        let relations = relationships.relations
+        for r in relations where r.0.name == "John" && r.1 == .parent {
+            print("John has a child called \(r.2.name)")
+        }
+    }
+}
+
+func main() {
+    let parent = Person("John")
+    let child1 = Person("Chris")
+    let child2 = Person("Matt")
+    
+    let relationships = Relationships()
+    relationships.addParentAndChild(parent, child1)
+    relationships.addParentAndChild(parent, child2)
+}
+
+main()
