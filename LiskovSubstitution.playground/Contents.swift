e360bbab97df4f65c9f285eb59006804f31bc4cd
mfsaglam <mfsaglam1@gmail.com>
2023-04-27 17:30:40 +0300

    breaking Liskov Substitution  principle in purpose, any instance of a parent class should be able to be replaced by an instance of a child class without affecting the correctness of the program.

---
+++ LiskovSubstitution.playground/Contents.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+class Rectangle: CustomStringConvertible {
+    var _width = 0
+    var _height = 0
+    
+    var width: Int {
+        get { return _width }
+        set(value) { _width = value }
+    }
+    
+    var height: Int {
+        get { return _height }
+        set(value) { _height = value }
+    }
+    
+    init() { }
+    
+    init(width: Int, height: Int) {
+        _width = width
+        _height = height
+    }
+    
+    var area: Int {
+        _width * _height
+    }
+    
+    public var description: String {
+        return "width: \(width), height: \(height)"
+    }
+}
+
+class Square: Rectangle {
+    override var width: Int {
+        get { return _width }
+        set(value) {
+            _width = value
+            _height = value
+        }
+    }
+    
+    override var height: Int {
+        get { return _height }
+        set(value) {
+            _width = value
+            _height = value
+        }
+    }
+}
+
+func setAndMeasure(_ rc: Rectangle) {
+    rc.width = 3
+    rc.height = 4
+    print("Expected area to be 12 but got \(rc.area)")
+}
+
+func main() {
+    let rc = Rectangle()
+    setAndMeasure(rc)
+    
+    let sq = Square()
+    setAndMeasure(sq)
+}
+
+main()
